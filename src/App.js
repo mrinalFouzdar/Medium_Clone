0b0969548ed403a7006764682052177433d6fe5c
mrinalFouzdar <100467560+mrinalFouzdar@users.noreply.github.com>
2022-04-04 03:58:17 +0530

    Our story page starting part

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import OurStorymain from "./Components/OurStoryPage/OurStorypart/OurStorymain";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <OurStorymain/>
+  </div>;
 }
 
 export default App;
