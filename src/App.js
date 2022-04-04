482bb30186f575d95c9ac8733ebfca85156dd83c
jaydeep2311 <100584963+jaydeep2311@users.noreply.github.com>
2022-04-04 16:33:33 +0530

    Made two components of Membership pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import OurStorymain from "./Components/OurStoryPage/OurStorymain";
+import Membership from "./Components/Membership/Membership";
 
 function App() {
-  return <div className="App">
-    <OurStorymain/>
-  </div>;
+  return (
+    <div className="App">
+      {/* <OurStorymain /> */}
+      <Membership />
+    </div>
+  );
 }
 
 export default App;
