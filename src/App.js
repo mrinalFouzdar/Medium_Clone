28b8fbb6c2538e3a2baac8c855df8e5a997f19bc
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-04-05 10:07:52 +0530

    Landing page and router installation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Landingpage } from "./Components/Landingpage/Landingpage.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Landingpage />
+    </div>
+  );
 }
 
 export default App;
